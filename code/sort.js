b863afe04e9adcff93b27c4930d83536a8be35ff
omittee <88842886+omittee@users.noreply.github.com>
2021-11-04 22:11:01 +0800

    Add files via upload

--- code/sort.js
+++ code/sort.js
@@ -1,74 +1,16 @@
-function animateSwap(obj1, obj2, flag, callback) {
-  var posX1 = obj1.offsetLeft;
-  var posY1 = obj1.offsetTop;
 
-  var posX2 = obj2.offsetLeft;
-  var posY2 = obj2.offsetTop;
-
-  clearInterval(obj1.timer);
-  clearInterval(obj2.timer);
-
-  obj1.timer = setInterval(function () {
-
-    let stepX1 = (posX2 - obj1.offsetLeft) / 10;
-    stepX1 = stepX1 > 0 ? Math.ceil(stepX1) : Math.floor(stepX1);
-
-    let stepY1 = (posY2 - obj1.offsetTop) / 10;
-    stepY1 = stepY1 > 0 ? Math.ceil(stepY1) : Math.floor(stepY1);
-
-    if (obj1.offsetLeft == posX2 && obj1.offsetTop == posY2) {
-      clearInterval(obj1.timer);
-      callback && callback();
-    }
-    obj1.style.left = obj1.offsetLeft + stepX1 + 'px';
-    obj1.style.top = obj1.offsetTop + stepY1 + 'px';
-
-  }, 10)
-
-  obj2.timer = setInterval(function () {
-    let stepX2 = (posX1 - obj2.offsetLeft) / 10;
-    stepX2 = stepX2 > 0 ? Math.ceil(stepX2) : Math.floor(stepX2);
-
-    let stepY2 = (posY1 - obj2.offsetTop) / 10;
-    stepY2 = stepY2 > 0 ? Math.ceil(stepY2) : Math.floor(stepY2);
-    if (obj2.offsetLeft == posX1 && obj2.offsetTop == posY1) {
-      clearInterval(obj2.timer);
-      callback && callback();
-    }
-
-    obj2.style.left = obj2.offsetLeft + stepX2 + 'px';
-    obj2.style.top = obj2.offsetTop + stepY2 + 'px';
-  }, 10)
-
-  if (!flag) {//flag控制是否假交换
-    window.setTimeout(function () {
-      const temp = obj1.innerText;
-      obj1.innerText = obj2.innerText;
-      obj2.innerText = temp;
-      obj1.style.left = posX1 + 'px';
-      obj1.style.top = posY1 + 'px';
-      obj2.style.left = posX2 + 'px';
-      obj2.style.top = posY2 + 'px';
-    }, [500]);
-  }
-
-}
+const array = [7, 3, 5, 4, 2, 1, 9, 10, 8, 6, 11];
+let len = array.length, s;//s是当前索引号
+let numAndFake = [];//动态添加元素的数组
+let step = [];//保存每次交换步骤
+let numArr = [];//保存DOM对象
+let auto;//一个计时器
 
 
-
-
-
-var array = [7, 3, 5, 4, 2, 1, 9, 10];
-var len = array.length, s;
-var numAndFake = [];
-numAndFake.push('<button class="init">初始化</button><button class="build">建堆</button><button class="autoplay">自动播放</button><button class="pre">上一步</button><button class="next">下一步</button><button class="end">结束</button>')
-for (let i = 0; i < len; i++) {
+for (let i = 0; i < len; i++) {//动态添加元素
   numAndFake.push('<div class="fake"></div><div class="num" id="id' + i + '"></div>');
 }
-document.body.innerHTML = numAndFake.join('');
-var step = [];//保存每次交换步骤
-var numArr = [];//保存DOM对象
-var counter = 0, auto;
+document.body.innerHTML += numAndFake.join('');
 
 for (let i = 0; i < array.length; i++) {
   numArr[i] = document.getElementById("id" + i);
@@ -76,7 +18,10 @@ for (let i = 0; i < array.length; i++) {
   numArr[i].style.backgroundColor = "green";
   numArr[i].style.borderColor = "green";
 }
-var fake = document.querySelectorAll('.fake');
+
+
+const fake = document.querySelectorAll('.fake');
+
 function origin() {
   for (let i = 0, move = 80, first = 30; i < array.length; i++) {
     numArr[i].style.left = first + i * move + "px";
@@ -90,34 +35,41 @@ function origin() {
     }
   }
 }
-let build = document.querySelector('.build');
-let autoplay = document.querySelector('.autoplay');
-let init = document.querySelector('.init');
-let pre = document.querySelector('.pre');
-let next = document.querySelector('.next');
-let end = document.querySelector('.end');
+
+const build = document.querySelector('.build');
+const autoplay = document.querySelector('.autoplay');
+const init = document.querySelector('.init');
+const pre = document.querySelector('.pre');
+const next = document.querySelector('.next');
+const end = document.querySelector('.end');
 init.addEventListener('click', function () {
   reinit();
 });
 
 //建堆动画
 build.addEventListener('click', function () {
-  origin();
+  origin();//先还原到建堆前
   next.disabled = true;
   pre.disabled = true;
   init.disabled = true;
   build.disabled = true;
   autoplay.disabled = true;
-  let temp = 0, t = 0;
+  let temp = 0, t = -1;//用来判断是否初始交换的索引号
   buildSwap = setInterval(function () {
-    animateSwap(numArr[temp], fake[temp], true);
+    animateSwap(numArr[temp], fake[temp], true);//真交换，入堆
     if (temp > 0) {
-      t = (temp - 1) / 2;
+      t = parseInt((temp - 1) / 2);//t是temp的父代
     }
-    if (t > 0 && numArr[t] < numArr[temp]) {
-      animateSwap(numArr[t], numArr[temp], false)
+    if (t >= 0 && parseInt(numArr[t].innerText) < parseInt(numArr[temp].innerText)) {
+      numArr[t].style.borderColor = "gold";
+      numArr[temp].style.borderColor = "gold";
+      setTimeout(function () {
+        animateSwap(numArr[t], numArr[temp - 1], false);//初始假交换
+        numArr[t].style.borderColor = "green";
+        numArr[temp - 1].style.borderColor = "green"
+      }, 500)
     }
-    if (temp == array.length - 1) {
+    if (temp == array.length - 1) {//结束入堆
       clearInterval(buildSwap);
       next.disabled = false;
       init.disabled = false;
@@ -132,14 +84,14 @@ build.addEventListener('click', function () {
 //自动播放
 
 autoplay.addEventListener('click', function () {
-  reinit(false);
+  reinit(false);//初始化
   auto = setInterval(function () {
     next.click();
-    if (numArr[0].style.backgroundColor == "lightgreen") {
+    if (numArr[0].style.backgroundColor == "lightgreen") {//堆顶变颜色则停止
       clearInterval(auto);
       pre.disabled = false;
       autoplay.disabled = false;
-      // console.log(step);
+      console.log(step);
     }
   }, 1200);
 })
@@ -148,23 +100,26 @@ autoplay.addEventListener('click', function () {
 end.addEventListener('click', function () {
   clearInterval(auto);
   clearInterval(buildSwap);
-  setInterval(function () {
+  setTimeout(function () {
     next.disabled = false;
     pre.disabled = false;
     build.disabled = false;
     init.disabled = false;
     reinit();
-  }, 500)
+  }, 500);
 })
 //初始化
-function reinit(flag = true) {//flag判断是否需要回到最初的起点
-  for (let i = 0; i < array.length; i++) {
-    numArr[i] = document.getElementById("id" + i);
-    numArr[i].innerText = array[i];
+function reinit(flag = true) {//flag判断是否需要回到最初的起点以及是否需要修改数据
+  for (let i = 0; i < array.length; i++) {/* 
+    numArr[i] = document.getElementById("id" + i); */
     numArr[i].style.backgroundColor = "green";
     numArr[i].style.borderColor = "green";
+
+    if (flag) {
+      numArr[i].innerText = array[i];
+    }
   }
-  len = 7;
+  len = array.length;
   s = Math.floor(len / 2) - 1;
   counter = 0;
   next.disabled = true;
@@ -190,9 +145,6 @@ pre.addEventListener("click", function () {
   step.pop();
   let tp2 = step[step.length - 1];
   step.pop();
-  if (counter > 0) {
-    counter--;
-  }
   if (s <= Math.floor(len / 2) - 1) {
     s++;
   }
@@ -202,7 +154,13 @@ pre.addEventListener("click", function () {
   if (tp1 == 0 && tp2 > 2 || tp2 == 0 && tp1 > 2) {
     len++;
   }
-  animateSwap(numArr[tp1], numArr[tp2], false);
+  if(tp1<=0&&tp2<=0&&tp1==tp2){
+    animateSwap(numArr[-tp1], fake[-tp2], true);
+  }
+  else{
+    animateSwap(numArr[tp1], numArr[tp2], false);
+  }
+  
   numArr[tp1].style.borderColor = "gold";
   numArr[tp2].style.borderColor = "gold";
   numArr[tp1].style.backgroundColor = "green";
@@ -227,22 +185,39 @@ next.addEventListener("click", function () {
   }
 
   heapify(s, ...numArr);
+
   s--;
   if (s < 0) {
-    next.addEventListener("click", topToBottom(), false);
+    next.addEventListener("click", topToBottom(fake), false);
     next.removeEventListener("click", topToBottom, false);
-    function topToBottom() {
+    function topToBottom(fake) {
+
       setTimeout(function () {
-        animateSwap(numArr[0], numArr[--len], false);
+        if(len>1){
+          animateSwap(numArr[0], numArr[--len], false);
+        }
+        else if(len==1){
+          len--;
+        }
+        else{
+          return;
+        }
         step[step.length] = 0;
         step[step.length] = len;
-      }, 500);
-      setTimeout(function () {
-        numArr[len].style.backgroundColor = "lightgreen";
-        numArr[len].style.borderColor = "lightgreen";
-      }, 1000);
-    }
 
+        setTimeout(function () {
+        
+          numArr[len].style.backgroundColor = "lightgreen";
+          numArr[len].style.borderColor = "lightgreen";
+          animateSwap(fake[len], numArr[len], true);
+          step[step.length++] = -len;
+          step[step.length++] = -len;
+          
+        }, 700);
+      }, 500);
+
+      
+    }
     s = Math.floor(len / 2) - 1;
   }
   setTimeout(function () {
@@ -255,8 +230,18 @@ next.addEventListener("click", function () {
 
 
 function heapify(i, ...arr) {
-  let left = 2 * i + 1;
-  let right = 2 * i + 2;
+  if (i < 0) {
+    return;
+  }
+  let left = i;
+  if (2 * i + 1 < arr.length) {
+    left = 2 * i + 1;
+  }
+  let right = left;
+  if (2 * i + 2 < arr.length) {
+    right = 2 * i + 2;
+  }
+
   largest = i;
   let arrLeft = parseInt(arr[left].innerText)
   let arrRight = parseInt(arr[right].innerText)
@@ -287,6 +272,72 @@ function heapify(i, ...arr) {
 
 
 
+function animateSwap(obj1, obj2, flag, callback) {
+
+  const posX1 = obj1.offsetLeft;
+  const posY1 = obj1.offsetTop;
+
+  const posX2 = obj2.offsetLeft;
+  const posY2 = obj2.offsetTop;
+
+  clearInterval(obj1.timer);
+  clearInterval(obj2.timer);
+
+  obj1.timer = setInterval(function () {
+    let stepX1 = (posX2 - obj1.offsetLeft) / 10;
+    stepX1 = stepX1 > 0 ? Math.ceil(stepX1) : Math.floor(stepX1);
+
+    let stepY1 = (posY2 - obj1.offsetTop) / 10;
+    stepY1 = stepY1 > 0 ? Math.ceil(stepY1) : Math.floor(stepY1);
+
+    if (obj1.offsetLeft == posX2 && obj1.offsetTop == posY2) {
+      clearInterval(obj1.timer);
+      callback && callback();
+    }
+    obj1.style.left = obj1.offsetLeft + stepX1 + 'px';
+    obj1.style.top = obj1.offsetTop + stepY1 + 'px';
+
+  }, 10)
+
+  obj2.timer = setInterval(function () {
+    let stepX2 = (posX1 - obj2.offsetLeft) / 10;
+    stepX2 = stepX2 > 0 ? Math.ceil(stepX2) : Math.floor(stepX2);
+    let stepY2 = (posY1 - obj2.offsetTop) / 10;
+    stepY2 = stepY2 > 0 ? Math.ceil(stepY2) : Math.floor(stepY2);
+    if (obj2.offsetLeft == posX1 && obj2.offsetTop == posY1) {
+      clearInterval(obj2.timer);
+      callback && callback();
+    }
+    obj2.style.left = obj2.offsetLeft + stepX2 + 'px';
+    obj2.style.top = obj2.offsetTop + stepY2 + 'px';
+  }, 10)
+
+  if (!flag) {//flag控制是否假交换
+    window.setTimeout(function () {
+      const temp = obj1.innerText;
+      obj1.innerText = obj2.innerText;
+      obj2.innerText = temp;
+      obj1.style.left = posX1 + 'px';
+      obj1.style.top = posY1 + 'px';
+      obj2.style.left = posX2 + 'px';
+      obj2.style.top = posY2 + 'px';
+    }, [500]);
+  }
+
+}
+/* 
+function animateSwap(obj1, obj2, flag, callback) {
+  const runkeyframes =` @keyframes ball-run{
+    to{
+        left: 0px;
+        top: 0px;
+    }
+  }`
+  const style = document.createElement('style');
+  style.innerHTML = runkeyframes;
+
+  document.getElementByTagName('head')[0].appendChild(style);
+} */
 
 // heapSort(...numArr);
 // [numArr[0],numArr[1],numArr[2],numArr[3],numArr[4],numArr[5],numArr[6]]
